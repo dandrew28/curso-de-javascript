633294db95afe4eb5d573e650597400585cb49e3
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-08-30 17:58:45 -0300

    function

---
+++ aula1.js
@@ -0,0 +1,20 @@
+//vamos fazer uma função que multiplica um valor//
+
+
+
+function multiplicaPorDois (valor) {
+return valor*2
+
+}
+console.log(multiplicaPorDois(5))
+
+
+// fazemos uma função que multiplica 10 por 8 e depois divide por 4//
+
+function multiOito (nomedovalor) {
+    return nomedovalor*8
+}
+console.log (multiOito(10))
+let resultado = multiOito(10)
+
+console.log (resultado / 4)
---
+++ trabalho.js
@@ -0,0 +1,9 @@
+let corSite = "amarelo"
+function mudarCor() {             //aqui diz que eu quero uma função que deixe a corSite apagada//
+corSite ="verde"
+        
+}
+
+console.log(corSite)
+mudarCor()
+console.log(corSite)
