d27e6db7d14bed5f8162d2b1a5b488829681faed
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-08-30 15:27:09 -0300

    Trabalho em javascript

---
+++ aula.js
@@ -0,0 +1,14 @@
+console.log('André')
+console.log('primeiro trabalho com javascript')
+
+// 1) variáveis//
+let nome = "André";
+console.log(nome);
+
+let idade = 10, altura = 1.80;
+console.log(idade);
+
+console.log(altura);
+
+const valorIngressoAdulto = 200
+console.log(valorIngressoAdulto)
