04c3782ed8edaddf67ea1ed520c886e3229bd247
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-08-30 16:57:02 -0300

    Modificações

--- aula.js
+++ aula.js
@@ -1,14 +1,61 @@
-console.log('André')
-console.log('primeiro trabalho com javascript')
+// console.log('André')
+// console.log('primeiro trabalho com javascript')
 
 // 1) variáveis//
-let nome = "André";
-console.log(nome);
 
-let idade = 10, altura = 1.80;
-console.log(idade);
+// let nome = "André";
+// console.log(nome);
 
-console.log(altura);
+// let idade = 10, altura = 1.80;
+// console.log(idade);
 
-const valorIngressoAdulto = 200
-console.log(valorIngressoAdulto)
+// console.log(altura);
+
+// const valorIngressoAdulto = 200
+// console.log(valorIngressoAdulto)
+
+//2 tipagem dinâmica//
+
+//let nome1 = "André";
+//let idade1 = 44;
+//let estaAprovado = true;
+//let sobrenome = undefined;
+//let corSelecionado = null;//
+
+
+//typeof nome1
+//typeof idade1
+//typeof estaAprovado
+//typeof sobrenome
+//typeof corSelecionado
+//console.log(typeof nome1)
+//console.log(typeof idade1)
+//console.log(typeof estaAprovado)
+//console.log(typeof sobrenome)
+//console.log(typeof corSelecionado)
+
+//estaAprovado = 20 //agora atribui que estaAprovado seja igual a um número, logo:
+//console.log(typeof estaAprovado)
+
+// transformar essas informações em objetos//
+let nome1 = "André";
+let idade1 = 44;
+let estaAprovado = true;
+let sobrenome = undefined;
+
+let pessoa = {
+    nome1: "André",
+    idade1: 44,
+    estaAprovado: true,
+    sobrenome: "de souza"
+}
+console.log (typeof pessoa)
+console.log (typeof pessoa.nome1)
+console.log (typeof pessoa.idade1)
+console.log (typeof estaAprovado)
+console.log (typeof sobrenome)
+
+console.log (pessoa.nome1)
+console.log (pessoa.idade1)
+console.log ( pessoa.estaAprovado)
+console.log ( pessoa.sobrenome)
---
+++ scripts.js
@@ -0,0 +1,16 @@
+//Arrays//
+
+let familia = [30, 40, 32, 17]
+
+console.log(familia)
+
+// o resultado na pagina será: 0 = 30  1=40  2=32   3=17//
+// para pegar apenas o número 40 temos que indicar a ordem dele (1)//
+
+console.log (familia[1])
+
+let rogerio = [45, "cancer", 1.60, "psicólogo", true]
+
+console.log (rogerio)
+
+console.log (rogerio[0])
