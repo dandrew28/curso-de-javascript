3bd6c55733840485666ce06ad31f7d01fbc487e8
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-09-02 09:33:10 -0300

    atualização

--- miniprojeto.js
+++ miniprojeto.js
@@ -2,11 +2,25 @@
 
 // Mini projeto: escrever uma função que usa 2 números e retorna o maior entre eles//
 
-let valormaximo = max(42,76);
-console.log (valormaximo);
+//let valormaximo = max(42,76);
+//console.log (valormaximo);
 
-function max(numero1,numero2) {
-    if (numero1 > numero2)
-    return numero1;
-   else return numero2;
-}
+//function max(numero1,numero2) {
+  //  if (numero1 > numero2)
+  //  return numero1;
+  // else return numero2;
+//}
+
+
+let valormaximo = max(65, 97)
+console.log(valormaximo)
+
+function max(numero1 , numero2) {
+    //if (numero1 > numero2)
+    //return numero1
+    //else return numero2
+                 // se sim
+ return   numero1 > numero2 ? numero1 : numero2 // ou utilizamos essa versão simplificada//
+}                          // se não
+
+
