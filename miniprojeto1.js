c8c2919e33d80c8cab7bb4769b2502852093460b
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-09-04 14:00:23 -0300

    miniprojeto1

--- miniprojeto1.js
+++ miniprojeto1.js
@@ -4,7 +4,7 @@
 // Não é divisível por 5 e 3 => entrada
 // Não é um número => Não é um número
 
-const resultado = fizzBuzz(43)
+const resultado = fizzBuzz(20)
 console.log(resultado)
 function fizzBuzz(entrada) {
     if (typeof entrada !== "number")
@@ -15,7 +15,7 @@ function fizzBuzz(entrada) {
         return "é divisível por 5"
     if (entrada % 3 === 0 && entrada % 5 === 0)
         return "é divisível por 3 e 5"
-    if (entrada % 3 !== 0 && entrada % 5 !== 0)
-        return "não é divisível por 3 e 5"
-
+   // if (entrada % 3 !== 0 && entrada % 5 !== 0) ESSA COMPARAÇÃO TEM QUE SER FEITA ANTES DE SE COMPARAR SE É DIVISÍVEL
+   //     return "não é divisível por 3 e 5"     // POR 3 E POR 5
+    return entrada
 }
