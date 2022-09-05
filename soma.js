474bdebbaa94e18b737d52c034367c57924acffb
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-09-05 07:00:50 -0300

    soma

---
+++ soma.js
@@ -0,0 +1,18 @@
+/* criar uma função de soma que retorne todos os múltiplos de 3 e 5*/
+
+
+
+// primeiro armazenar os múltiplos de 3
+// depois armazenar os míltiplos de 5
+// por último somar os dois
+soma(10);
+function soma(numero) {
+
+for (let i=0; i <= numero; i++) {
+    if (numero % 3 === 0)
+    console.log (i)
+    
+}
+
+}
+
