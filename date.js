5db89968787f96d81d7384c50fd9b116edc7325d
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-09-06 07:15:24 -0300

    data

---
+++ date.js
@@ -0,0 +1,12 @@
+// maneiras de indicar a data
+
+
+
+const data = new Date()
+console.log(data)
+
+const data1 = new Date (2022,08,06,07,10)
+console.log(data1)
+
+const data2 = new Date ('september 06 2022 07:13')
+console.log(data2)
