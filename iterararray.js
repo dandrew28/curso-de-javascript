ce9a84c688f91cb7cec1dd969d828d241923ae52
André Luiz da Fonseca Medeiros <andre_aceofbase@yahoo.com.br>
2022-09-07 15:33:40 -0300

    join split

--- iterararray.js
+++ iterararray.js
@@ -4,3 +4,12 @@ const numeros = [1,2,3,4,5,6]
 for(numero of numeros)
 console.log(numero)
 
+// agora vamos usar o split e depois o join para uma variável 
+
+const frase = "O país é uma super potência mundial"
+
+const separar = frase.split(" ")
+console.log(separar)
+
+//agora vamos juntar
+console.log(separar.join ("-"))
